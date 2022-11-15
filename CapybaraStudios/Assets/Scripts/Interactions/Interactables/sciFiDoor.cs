91b05c412130962dfe6b0ceb8ef0d65d3285ddd1
s50312 <s50312@bht-berlin.de>
2022-11-16 00:25:26 +0100

    first theme done

---
+++ CapybaraStudios/Assets/Scripts/Interactions/Interactables/sciFiDoor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sciFiDoor : Interactable
+{
+    public GameObject button;
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void Interact()
+    {
+        Debug.Log("Interacted with " + button.name);
+        var buttonRenderer = button.GetComponent<Renderer>();
+        if(buttonRenderer.material.color.Equals(Color.red)) {
+            buttonRenderer.material.SetColor("_Color", Color.green);
+            leftDoor.transform.localPosition = new Vector3(0, 0, 3);
+            rightDoor.transform.localPosition = new Vector3(0, 0, -4);
+        }
+        else {
+            buttonRenderer.material.SetColor("_Color", Color.red);
+            leftDoor.transform.localPosition = new Vector3(0, 0, 0);
+            rightDoor.transform.localPosition = new Vector3((float)0.007935028, (float)-0.1499473, (float)-1.433132);
+        }
+    }
+}
