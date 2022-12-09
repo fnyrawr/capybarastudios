0358ff561152203715e42c4608232f0270847525
s51052 <heimleiter.dasheim@gmail.com>
2022-12-09 22:22:23 +0100

    prefab change

--- CapybaraStudios/Assets/Scripts/AI/AIAgentConfig.cs
+++ CapybaraStudios/Assets/Scripts/AI/AIAgentConfig.cs
@@ -7,5 +7,5 @@ public class AIAgentConfig : ScriptableObject
 {
     public float maxTime = 1f;
     public float maxDistance = 1f;
-    public float maxSightDistance = 10f;
+    public float maxSightDistance = 5f;
 }
