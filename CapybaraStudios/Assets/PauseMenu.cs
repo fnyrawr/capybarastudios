322bbf21b341ba1269ae6800e1adcb3f76de740a
adrian <adrianjost1@gmx.de>
2022-12-19 15:54:54 +0100

    hud prefab adjustments

--- CapybaraStudios/Assets/PauseMenu.cs
+++ CapybaraStudios/Assets/PauseMenu.cs
@@ -5,19 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-   public static bool GameIsPaused = false;
+    public static bool GameIsPaused = false;
 
-   public GameObject pauseMenuUI;
+    public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if(GameIsPaused)
+            if (GameIsPaused)
             {
                 Resume();
-            } else
+            }
+            else
             {
                 Pause();
             }
