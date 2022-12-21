8f797a69f53359fc5faea56fcdafcee398460119
adrian_ <adrianjost1@gmx.de>
2022-12-22 00:58:10 +0100

    tab controller revert

--- CapybaraStudios/Assets/Scripts/TabController.cs
+++ CapybaraStudios/Assets/Scripts/TabController.cs
@@ -18,16 +18,8 @@ public class TabController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-
-
-        //timeText.text = Time.timeSinceLevelLoad + "";
-        timeText.text = string.Format("{0:00}", Time.timeSinceLevelLoad);
-        
-=======
         timeText.text = FindObjectOfType<GameManager>().time;
         damageText.text = GameManager.damageDone + "";
         killsText.text = GameManager.kills + "";
->>>>>>> f504a30633903a588fe8f9b925528e5b5051748d
     }
 }
