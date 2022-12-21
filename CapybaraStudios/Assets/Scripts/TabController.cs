9245b392d152b8959571a482d55fb6d5491ac633
adrian <adrianjost1@gmx.de>
2022-12-22 00:02:14 +0100

    hud changes

--- CapybaraStudios/Assets/Scripts/TabController.cs
+++ CapybaraStudios/Assets/Scripts/TabController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TabController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+
+    public TextMeshProUGUI damageText;
+    public TextMeshProUGUI killsText;
+    public TextMeshProUGUI timeText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeText.text = Time.timeSinceLevelLoad + "";
     }
 }
