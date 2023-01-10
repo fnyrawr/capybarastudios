d9d91de84e4c5aacf0f60019bae1df75ee8d333a
Papierkorb <felix.abraham@campus.tu-berlin.de>
2023-01-10 16:22:57 +0100

    launcher knockback added + bouncy grenades

--- CapybaraStudios/Assets/Scripts/Weapons/Launcher.cs
+++ CapybaraStudios/Assets/Scripts/Weapons/Launcher.cs
@@ -7,11 +7,18 @@ public class Launcher : MonoBehaviour
     public Transform firePoint;
     public GameObject rocket;
 
+    public float knockbackForce = 100f;
+
     public float range = 100f;
 
     public void Launch()
     {
         GameObject rocketInstance = Instantiate(rocket, firePoint.position, firePoint.rotation);
         rocketInstance.GetComponent<Rigidbody>().AddForce(firePoint.forward * range, ForceMode.Impulse);
+    }
+
+    public float GetKnockbackForce()
+    {
+        return knockbackForce;
     }
 }
